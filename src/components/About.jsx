8d2db8fd94b4a3fcce9fba54f4cadf65b32c55ed
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-08-16 15:41:29 +0400

    quotes of the day and about

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div>
+          <p>About</p>
+        </div>
+        <p>
+          Having cultivated a keen eye for detail, I take pride in crafting
+          visually appealing and responsive websites. However, my expertise
+          extends beyond aesthetics – I possess a strong grasp of back-end
+          development, allowing me to build robust and efficient server-side
+          functionalities. What sets me apart is my unwavering commitment to
+          refining my craft. I approach challenges with a solution-oriented
+          mindset, leveraging my problem-solving skills to overcome obstacles
+          and deliver optimal results. My ability to collaborate effectively
+          within development teams underscores my belief in the power of shared
+          knowledge and cohesive efforts. As I continue my journey, my appetite
+          for knowledge remains insatiable. My focus is on embracing emerging
+          technologies, leveraging cutting-edge frameworks, and pushing the
+          boundaries of what is achievable in web development. With a passion
+          for innovation and a drive to excel, I am poised to create
+          transformative web solutions that leave a lasting impact.
+        </p>
+        <br />
+
+        <p>
+          "The only limit to our realization of tomorrow will be our doubts of
+          today." – Franklin D. Roosevelt <br /> "Success is not final, failure
+          is not fatal: It is the courage to continue that counts." – Winston
+          Churchill <br />
+          "The future belongs to those who believe in the beauty of their
+          dreams." – Eleanor Roosevelt <br />
+          "The only way to do great work is to love what you do." – Steve Jobs{" "}
+          <br />
+          "The journey of a thousand miles begins with one step." – Lao Tzu
+          <br /> "In the midst of difficulty lies opportunity." – Albert
+          Einstein <br /> "The expert in anything was once a beginner." – Helen
+          Hayes <br /> "Learning never exhausts the mind." – Leonardo da Vinci
+          <br /> "The harder the conflict, the greater the triumph."– George
+          Washington <br /> "Don’t watch the clock; do what it does. Keep
+          going." – Sam Levenson <br /> "Your work is going to fill a large part
+          of your life, and the only way to be truly satisfied is to do what you
+          believe is great work." – Steve Jobs <br /> "Believe you can and
+          you're halfway there." – Theodore Roosevelt <br /> "The secret of
+          getting ahead is getting started." – Mark Twain <br /> "Success is not
+          the key to happiness. Happiness is the key to success. If you love
+          what you are doing, you will be successful." – Albert Schweitzer
+          <br />
+          "Continuous effort, not strength or intelligence, is the key to
+          unlocking our potential." – Winston Churchill
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
